4c74a80be29a2aa51e479b43f21083ba7bbfe2e9
starzkeeper <yaroslav020304@gmail.com>
2022-12-20 11:28:03 +0300

    second commit

--- movies/models.py
+++ movies/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 
 class Movie(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=200)
     description = models.TextField()
 
     def __str__(self):
